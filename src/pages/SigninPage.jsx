d9928c4211610d786bdaabc3909983891025860e
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2024-03-13 22:07:55 -0300

    [M03S07] Ex 3 - Vincular as telas de login  um useContext de usuário exportando os métodos login, logout e a variável user que conterá as informações públicas do usuário

--- src/pages/SigninPage.jsx
+++ src/pages/SigninPage.jsx
@@ -1,14 +1,32 @@
 import { useState } from "react"
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from '../context/AuthContext.jsx'
 
 function SigninPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate()
+  const { login } = useAuth()
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      if ((username || username.length > 0) && (password || password.length > 0)) {
+        let userData = { username: username, password: password };
+        login(userData);
+        navigate('/home');
+      } else {
+        alert('Usuário ou senha inválido.')
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div>
       <h1>Signin</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="w-150">
           <input placeholder="Username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
           <input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
