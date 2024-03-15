31ec4d906f47c648f790ca33be0df734963302f2
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2024-03-15 16:31:36 -0300

    [M03S07] Ex 6 - Adicione um botão de logout no header, que ficará presente apenas na parte protegida da aplicação, e conecte com o logout de userContext

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,21 @@
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
 function HeaderComponent() {
+    const { logout } = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        navigate('/signin');
+    }
 
     return (
         <div className="header">
             <h2>Header</h2>
+            <div className="button-logout">
+                <button onClick={handleLogout}>Logout</button>
+            </div>
         </div>
     )
 }
