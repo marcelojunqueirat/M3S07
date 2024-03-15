34d778c269bb71940f7011439a3fa7960b465e17
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2024-03-15 16:24:06 -0300

    [M03S07] Ex 5 - Adicione rotas para chamar este novo layout da aplicação. Aplique ainda proteção de rotas seguindo o useContext do usuário

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { BrowserRouter as Router } from 'react-router-dom'
-import './App.css'
-import RoutesComponent from './routes/Routes'
-import HeaderComponent from './components/Header'
-import SidebarComponent from './components/Sidebar'
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+import RoutesComponent from './routes/Routes';
+import HeaderComponent from './components/Header';
+import SidebarComponent from './components/Sidebar';
+import { useAuth } from './context/AuthContext.jsx';
 
 function App() {
+  const { isAuthenticated } = useAuth()
 
   return (
     <>
       <Router>
-        <div className="grid-container">
-          <HeaderComponent />
-          <SidebarComponent />
+        {isAuthenticated ? (
+          <div className="grid-container">
+            <HeaderComponent />
+            <SidebarComponent />
+            <RoutesComponent />
+          </div>
+        ) : (
           <RoutesComponent />
-        </div>
+        )}
       </Router>
     </>
   )
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,18 +2,36 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import SigninPage from '../pages/SigninPage';
 import SignupPage from '../pages/SignupPage';
 import HomePage from '../pages/HomePage';
+import { useAuth } from '../context/AuthContext';
 
 export default function RoutesComponent() {
+  const { isAuthenticated } = useAuth();
+
+  const loginRedirect = (children) => {
+    if (isAuthenticated) {
+      return <Navigate to='/home' replace />
+    }
+    return children;
+  }
 
   return (
     <Routes>
-      <>
-        <Route path='/signin' Component={SigninPage} />
-        <Route path='/signup' Component={SignupPage} />
-        <Route path='/' exact Component={HomePage} />
-        <Route path='/home' Component={HomePage} />
+      <Route path='/signin' element={
+        loginRedirect(<SigninPage />)
+      } />
+      <Route path='/signup' element={
+        loginRedirect(<SignupPage />)
+      } />
+
+      {isAuthenticated ? (
+        <>
+          <Route path='/' exact Component={HomePage} />
+          <Route path='/home' Component={HomePage} />
+        </>
+
+      ) : (
         <Route path='*' element={<Navigate replace to='/signin' />} />
-      </>
+      )}
     </Routes>
   )
 }
